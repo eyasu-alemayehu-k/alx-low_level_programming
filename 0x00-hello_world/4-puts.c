b3ccfd1e73086cad50729f0659370bad07e4d5ea
liyu-cpu <eyasualemayehukuma@gmail.com>
2022-12-08 04:36:37 -0800

    commiting task of c programming project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main -Entery point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
