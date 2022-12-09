04ee2fa724467ed845cb2c662099325ef34c1df8
liyu-cpu <eyasualemayehukuma@gmail.com>
2022-12-09 06:38:07 -0800

    commiting task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,8 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entery point
+ *
+ * Return: Always 0 (success{
+ */
+
 int main(void)
 {
 	int n;
