fbd8d0de95d89f6f8e8aa36cd56c5ba6c9fde0ec
liyu-cpu <eyasualemayehukuma@gmail.com>
2022-12-09 09:22:59 -0800

    commiting task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+        char c;
+
+        c = 'a';
+        while (c <= 'z')
+        {
+                if ( c != 'e' && c != 'q')
+                {
+                putchar(c);
+                }
+                c++;
+        }
+        putchar('\n');
+        return (0);
+}
