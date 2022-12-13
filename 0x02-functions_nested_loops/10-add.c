e859a25ffca24906ded785147953c77a96b190e9
liyu-cpu <eyasualemayehukuma@gmail.com>
2022-12-13 09:56:47 -0800

    commiting task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - add to integres
+ * @num1 : first number
+ * @num2 : second number
+ * Return: sum of num1 and num2
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
