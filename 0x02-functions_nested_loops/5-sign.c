0db463d09293e001915f8c13dfad68f7bd718ff2
liyu-cpu <eyasualemayehukuma@gmail.com>
2022-12-13 04:27:36 -0800

    commiting task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of a number
+ * @n : operand number
+ * Return: 1 if n is greater than zero 0 and prints 0 if n is zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('+');
+		return (1);
+	else if (n == 0)
+		_putchar('0');
+		return (0);
+	_putchar('-');
+	return (-1);
+}
