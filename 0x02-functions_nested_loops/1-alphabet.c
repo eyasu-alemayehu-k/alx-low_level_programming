47d3920b79ad5c42188c68abfeaee0d489a20b2f
liyu-cpu <eyasualemayehukuma@gmail.com>
2022-12-13 03:04:55 -0800

    commiting task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  *
- * Return: always 0 (success)
+ * Return: 0
  */
-int main(void)
+void print_alphabet(void)
 {
 	char c;
 
