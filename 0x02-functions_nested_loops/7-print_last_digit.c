9d9137ffde015dd5a512133d601c1cd2cb1eab16
liyu-cpu <eyasualemayehukuma@gmail.com>
2022-12-13 05:26:06 -0800

    commiting task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,19 +10,19 @@ int print_last_digit(int n)
 	if (n > 0)
 	{
 		n %= 10;
-		putchar('0' + n);
+		_putchar('0' + n);
 		return (n);
 	}
 	else if (n == 0)
 	{
-		putchar('0' + 0);
+		_putchar('0' + 0);
 		return (0);
 	}
 	else
 	{
 		n *= -1;
 		n %= 10;
-		putchar('0' + n);
+		_putchar('0' + n);
 		return (n);
 	}
 }
