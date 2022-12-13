067edb3f437f206abff298c5c6f403a133fb74c3
liyu-cpu <eyasualemayehukuma@gmail.com>
2022-12-13 09:31:45 -0800

    commiting task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 times table
+ *
+ * return: times table
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (((i * j) / 10) == 0)
+			{
+				_putchar(' ');
+				_putchar('0' + ((i * j) % 10));
+			}
+			else
+			{
+				_putchar('0' + ((i * j) / 10));
+				_putchar('0' + ((i * j) % 10));
+			}
+			if (c < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
