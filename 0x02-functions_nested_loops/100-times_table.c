50d4d57c13e9df35241c57478b3c182d4f67d121
liyu-cpu <eyasualemayehukuma@gmail.com>
2022-12-13 12:27:45 -0800

    commiting task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,38 +1,48 @@
 #include "main.h"
 
+
 /**
- * print_times_table - print the n times table
- * @n : integer number
- * Return: times table with n range
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
  */
 void print_times_table(int n)
 {
-	int i;
-	int j;
+	int i, j, k;
 
-	for (i = 0; i < n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j < n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			if (((i * j) / 10) == 0)
+			for (j = 0; j <= n; j++)
 			{
-				if (j != 0)
+				k = j * i;
+				if (j == 0)
 				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
-				_putchar('0' + ((i * j) % 10));
 			}
-			else
-			{
-				_putchar('0' + ((i * j) / 10));
-				_putchar('0' + ((i * j) % 10));
-			}
-			if (j < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
