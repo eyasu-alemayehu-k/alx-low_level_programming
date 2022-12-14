2559a613129d3ea214b7e1497000259f221942a8
liyu-cpu <eyasualemayehukuma@gmail.com>
2022-12-14 09:16:26 -0800

    commiting task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,27 +15,17 @@ int largest_number(int a, int b, int c)
 	if (a > b)
 	{
 		if (b > c)
-		{
 			largest = a;
-		}
 		else if (a > c)
-		{
 			largest = a;
-		}
 		else
-		{
 			largest = c;
-		}
 	else
 	{
 		if (b < c)
-		{
 			largest = c;
-		}
 		else
-		{
 			largest = b;
-		}
 	}
 	return (largest);
 }
