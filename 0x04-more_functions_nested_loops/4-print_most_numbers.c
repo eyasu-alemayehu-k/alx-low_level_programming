0edeaed14ae49671d271f36c9fb929de94bc2a24
liyu-cpu <eyasualemayehukuma@gmail.com>
2022-12-15 11:26:19 -0800

    commiting task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,11 +9,13 @@ void print_most_numbers(void)
 {
 	int n;
 
-	n = 0;
-	while (n < 10)
+	n = 48;
+	while (n < 58)
 	{
-		if (n != 2 && n != 4)
-			_putchar('0' + n);
+		if (n != 50 && n != 52)
+		{
+			_putchar(n);
+		}
 	n++;
 	}
 	_putchar('\n');
