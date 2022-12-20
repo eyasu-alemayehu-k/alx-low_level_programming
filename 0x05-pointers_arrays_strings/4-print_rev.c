778f161fda4447c35535da07307cfde2943e64c6
liyu-cpu <eyasualemayehukuma@gmail.com>
2022-12-20 00:30:50 -0800

    commiting task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_rev - print string revers.
+* @s : array pointer
+* Return : string
+*/
+void print_rev(char *s)
+{
+	int len, i;
+
+	len = 0;
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+	for (i = len; i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
