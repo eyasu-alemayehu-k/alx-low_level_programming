b0f0e11645ffbf6388feb83ac3b8c65e770eab56
liyu-cpu <eyasualemayehukuma@gmail.com>
2023-01-07 03:40:42 -0800

    commiting first task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,14 @@
-#include "main.h"
 #include <stdio.h>
-
+#include "main.h"
 /**
- * main - print name of the program
+ * main - prints the name of the program
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: Always 0 (success)
+ *
+ * Return: Always 0 (Success)
  */
-
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 	return (0);
 }
